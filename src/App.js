ef993833ded69286c6bee18658e6f0e13c9ea618
sbabureddy <sbabureddy01@gmail.com>
2020-12-16 16:40:49 +0530

    Add App.test.js file to Test covarage

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import Hello from './components/Hello';
 import ApiCall from './components/ApiCall';
@@ -6,6 +5,14 @@ import ApiCall from './components/ApiCall';
 function App() {
   return (
     <div className="App">
+      <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
      <Hello name="React"/>
      <ApiCall id="1" />
     </div>
