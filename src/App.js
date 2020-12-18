6c8f7dce810083cb29efadeb8302a1634f0b4e39
sbabureddy <sbabureddy01@gmail.com>
2020-12-18 12:59:49 +0530

    Test Jest Timer

--- src/App.js
+++ src/App.js
@@ -1,18 +1,32 @@
+/* eslint-disable no-unused-vars */
 
 import './App.css';
 import Hello from './components/Hello';
 import ApiCall from './components/ApiCall';
 import Toggle from './components/Toggle';
 import { useState } from 'react';
+import Card from './components/Card';
 
 function App() {
   
   // eslint-disable-next-line no-unused-vars
   const [state, setState] = useState(false)
+  
+  const [select, setSelect] = useState(null)
+
+  // onChange for Toggle component 
   const onChange = (state) => {
     setState(state => !state)
   }
+
+  // onSelect for Card Component
+  const onSelect = (e) => {
+    console.log(e)
+    
+    // setSelect(e.target.value)
+  }
   
+
   return (
     <div className="App">
       <a
@@ -26,6 +40,8 @@ function App() {
      <Hello name="React"/>
      <ApiCall id="1" />
      <Toggle onChange={onChange} />
+     <br />
+     <Card onSelect={onSelect} />
     </div>
   );
 }
